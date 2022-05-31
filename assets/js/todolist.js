061349664735ec6846b70eb039827cf69c1d6c8f
Paula Lopes <pauladev21@gmail.com>
2022-05-31 13:03:30 -0300

    cleanChecked was corrected. cleanAll implemented. Issue with warnings

--- assets/js/todolist.js
+++ assets/js/todolist.js
@@ -2,6 +2,8 @@ const CHECKED = true, UNCHECKED = false;
 let taskList = [["Sample task1", CHECKED], ["sample task2", UNCHECKED]];
 
 function createTaskListHtml(){
+    cleanWarnings();
+   
     let task, txtWrapper, ckBox, lbl;
     output = '';
     taskListElem = document.getElementById('task-list');
@@ -21,6 +23,7 @@ function createTaskListHtml(){
         ckBox.setAttribute('type','checkbox');
         ckBox.setAttribute('class', 'task-checkbox');
         ckBox.setAttribute('id', task);
+        ckBox.addEventListener('change', function() {updateTaskList(i);})
         if (taskList[i][1] == CHECKED){
             ckBox.setAttribute('checked', 'checked')
         }
@@ -39,10 +42,13 @@ function createTaskListHtml(){
 function addTask(){
     newTask = document.getElementsByClassName('new-task')[0];
     if (newTask.value.trim() === ''){
-        alert('You need to describe your task before adding it!');
+        warningElement = document.getElementById('empty-new-task-warning');
+        warningElement.setAttribute('visibility', 'visible')
+        warningElement.innerHTML = "You need to describe the task to add it to the list!"
         return;
     }
     taskList.push([newTask.value, UNCHECKED]);
+    document.getElementById('task-list').innerHTML='';
     createTaskListHtml();
     newTask.value = '';
 }
@@ -50,4 +56,51 @@ function addTask(){
 function clickButton(event){
     if (event.keyCode === 13)
         document.getElementById('new-task-button').click();
+}
+
+function cleanDone(allDone = false){
+    let keepingTasks = [];
+    let tasksWrapper = document.getElementsByClassName('task-wrapper')
+    removed=0;
+    console.log('n tasks = ' + tasksWrapper.length)
+    
+    initialLength = tasksWrapper.length
+    for (let i = 0; i < initialLength; i++){
+        console.log(i)
+        if (taskList[i][1] == CHECKED || allDone){
+            console.log('index = ' + i + ' state = ' + taskList[i][1]);
+            tasksWrapper[i-removed].remove(); 
+            removed++;
+        }
+        else{
+            console.log(taskList[i])
+            keepingTasks.push(taskList[i]);
+        }
+    }
+    if (tasksWrapper.length == 0){
+        tasksListElem = document.getElementById('task-list');
+        taskListElem.innerHTML = "No tasks left to be done!"
+    }
+    taskList = keepingTasks;
+    console.log(taskList)
+    createTaskListHtml();
+}
+
+function updateTaskList(index){
+    console.log('BEFORE: index = ' + index + ' state = ' + taskList[index][1]);
+    taskList[index][1] = !taskList[index][1];
+    console.log('AFTER: index = ' + index + ' state = ' + taskList[index][1]);
+}
+
+function cleanAll(){
+    cleanDone(allDone = true);
+}
+
+function cleanWarnings(){
+    warnings = document.getElementsByClassName('warning');
+    for (let i=0; i< warnings.length; i++){
+        warnings[i].innerHTML = '';
+        warnings[i].setAttribute('visibility', 'hidden');
+    }
+
 }
